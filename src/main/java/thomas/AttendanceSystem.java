202ab930d3417da48aba8feece5421ca99f8c4e6
Thomas Abas <abasienyenethomas2@gmail.com>
2025-09-04 17:10:01 +0100

    updates

--- src/main/java/thomas/AttendanceSystem.java
+++ src/main/java/thomas/AttendanceSystem.java
@@ -1,4 +1,19 @@
 package thomas;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class AttendanceSystem {
+    List<Student> student;
+
+    public void addStudent(int id, String name){
+
+    }
+    public void markAttendance(int studentId, LocalDate date, AttendanceStatus status){
+
+    }
+    public void viewAttendance(String studentId){
+
+    }
+
 }
--- src/main/java/thomas/Student.java
+++ src/main/java/thomas/Student.java
@@ -15,6 +15,7 @@ public class Student {
         attendanceRecords = new HashMap<>();
     }
     private Map<LocalDate, AttendanceStatus>attendanceRecords;
+
     public void markAttendance(LocalDate date, AttendanceStatus status){
         attendanceRecords.put(date, status);
     }
@@ -22,17 +23,17 @@ public class Student {
         return attendanceRecords.get(date);
     }
 
-    public static void main(String[] args) {
-        Student s1 = new Student("001", "Thomas");
-        Student s2 = new Student("001", "Thomas");
-
-        s1.markAttendance(LocalDate.of(2025, 9, 3), AttendanceStatus.Present);
-        s2.markAttendance(LocalDate.of(2025, 9, 3), AttendanceStatus.Present);
-
-        System.out.println(s1.getAttendanceForDate(LocalDate.of(2025, 9, 3))); // Present
-        System.out.println(s2.getAttendanceForDate(LocalDate.of(2025, 9, 3))); // null
-
-    }
+//    public static void main(String[] args) {
+//        Student s1 = new Student("001", "Thomas");
+//        Student s2 = new Student("001", "Thomas");
+//
+//        s1.markAttendance(LocalDate.of(2025, 9, 3), AttendanceStatus.Present);
+//        s2.markAttendance(LocalDate.of(2025, 9, 3), AttendanceStatus.Present);
+//
+//        System.out.println(s1.getAttendanceForDate(LocalDate.of(2025, 9, 3))); // Present
+//        System.out.println(s2.getAttendanceForDate(LocalDate.of(2025, 9, 3))); // null
+//
+//    }
 
 
 }
